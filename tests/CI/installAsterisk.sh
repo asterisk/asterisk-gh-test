d19d769807b5e261fad828154b7488a1c14004b1
George Joseph <gjoseph@sangoma.com>
2023-03-13 18:14:00 -0600

    CI: Make build and install scripts github friendly

--- tests/CI/installAsterisk.sh
+++ tests/CI/installAsterisk.sh
@@ -4,6 +4,8 @@ CIDIR=$(dirname $(readlink -fn $0))
 GITHUB=0
 UNINSTALL=0
 UNINSTALL_ALL=0
+OUTPUT_DIR=/tmp/asterisk_ci/
+
 source $CIDIR/ci.functions
 
 MAKE=`which make`
@@ -22,24 +24,29 @@ destdir=${DESTDIR:+DESTDIR=$DESTDIR}
 declare -p _version
 declare -p destdir
 
+echo "::notice::Uninstalling exisitng build"
+begin_log ${OUTPUT_DIR}/uninstall
+(
 [ $UNINSTALL -gt 0 ] && ${MAKE} ${destdir} uninstall
 [ $UNINSTALL_ALL -gt 0 ] && ${MAKE} ${destdir} uninstall-all
+) >>"$log_out" 2>>"$err_out"  || { echo "::error::Uninstall failed.  See ${err_to} for more details." ; exit 1 ; }
+end_log
 
-if [ x"$OUTPUT_DIR" != x ] ; then
-	log_to=${OUTPUT_DIR}/install.log
-else
-	log_to=/dev/stdout
-fi
-
-${MAKE} ${destdir} install > "${log_to}" || ${MAKE} ${destdir} NOISY_BUILD=yes install > "${log_to}" || exit 1
-${MAKE} ${destdir} samples install-headers >> "${log_to}"
-if [ x"$DESTDIR" != x ] ; then
-	sed -i -r -e "s@\[directories\]\(!\)@[directories]@g" $DESTDIR/etc/asterisk/asterisk.conf
-	sed -i -r -e "s@ /(var|etc|usr)/@ $DESTDIR/\1/@g" $DESTDIR/etc/asterisk/asterisk.conf
-fi
+echo "::notice::Installing"
+begin_log ${OUTPUT_DIR}/install
+(
+	${MAKE} ${destdir} install || ${MAKE} ${destdir} NOISY_BUILD=yes install || exit 1
+	${MAKE} ${destdir} samples install-headers 
+	if [ x"$DESTDIR" != x ] ; then
+		sed -i -r -e "s@\[directories\]\(!\)@[directories]@g" $DESTDIR/etc/asterisk/asterisk.conf
+		sed -i -r -e "s@ /(var|etc|usr)/@ $DESTDIR/\1/@g" $DESTDIR/etc/asterisk/asterisk.conf
+	fi
+) >>"${log_to}" 2>>"${err_to}" || { echo "::error::Install failed.  See ${err_to} for more details." ; exit 1 ; }
+end_log
 
 set +e
 if [ x"$USER_GROUP" != x ] ; then
+	echo "::notice::Setting permissions"
 	chown -R $USER_GROUP $DESTDIR/var/cache/asterisk
 	chown -R $USER_GROUP $DESTDIR/var/lib/asterisk
 	chown -R $USER_GROUP $DESTDIR/var/spool/asterisk
